ffd65ad257e31f949e63fef7c25e1805e2f1f3cb
Jeffrey Varner <jdv27@cornell.edu>
2023-09-23 08:09:25 -0400

    initial submit
    initial submit

---
+++ Include.jl
@@ -0,0 +1,34 @@
+# setup the paths -
+const _ROOT = pwd();
+const _PATH_TO_SRC = joinpath(_ROOT, "src");
+const _PATH_TO_DATA = joinpath(_ROOT, "data");
+
+# download external packages
+using Pkg;
+Pkg.add(path="https://github.com/varnerlab/VLDecisionsPackage.jl.git")
+Pkg.activate("."); Pkg.instantiate(); Pkg.update()
+
+# load external packages -
+using VLDecisionsPackage
+using CSV
+using DataFrames
+using Plots
+using Colors
+using Distributions
+using JLD2
+using FileIO
+using Statistics
+using LinearAlgebra
+using MathOptInterface
+using DataStructures
+
+# load my color palette -
+colors = Dict{Int,RGB}();
+colors[1] = colorant"#0077BB"
+colors[2] = colorant"#88CCEE"
+colors[3] = colorant"#EE7733"
+colors[4] = colorant"#CC3311"
+
+# load my codes -
+include(joinpath(_PATH_TO_SRC, "Compute.jl"));
+include(joinpath(_PATH_TO_SRC, "Files.jl"));
